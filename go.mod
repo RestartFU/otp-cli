9ff7e8fceb44ffb0c3063521cd97865ea3d814a6
The One And Only Restart <you@example.com>
2024-09-29 15:44:13 -0400

    added scan command

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.23.1
 
 require (
 	github.com/joho/godotenv v1.5.1
+	github.com/makiuchi-d/gozxing v0.1.1
 	golang.org/x/term v0.24.0
 )
 
 require (
-	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 )
--- internal/command/scan.go
+++ internal/command/scan.go
@@ -1,1 +1,103 @@
 package command
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/makiuchi-d/gozxing"
+	"github.com/makiuchi-d/gozxing/qrcode"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"os"
+	"otp/internal/crypto"
+	"otp/internal/otp"
+	"otp/internal/term"
+)
+
+type Scan struct {
+	Service string
+	Path    string
+}
+
+func (s Scan) Run() error {
+	buf, err := os.ReadFile(dataPath)
+	if err != nil {
+		return err
+	}
+
+	prefix := "Password: "
+	if len(buf) <= 0 {
+		prefix = "Initialize " + prefix
+	}
+
+	ko := make(map[string]string)
+	psw, _ := term.StdinPassword(prefix)
+
+	if len(buf) > 0 {
+		content, err := crypto.Decrypt(psw, string(buf))
+		if err != nil {
+			return ErrWrongPassword
+		}
+		ko, err = godotenv.Unmarshal(content)
+		if err != nil {
+			return ErrWrongPassword
+		}
+
+		_, ok := ko[s.Service]
+		if ok {
+			return ErrServiceExists
+		}
+	} else {
+		confirmation, _ := term.StdinPassword("Confirm Password: ")
+		if confirmation != psw {
+			return ErrPasswordNotMatch
+		}
+	}
+
+	secret := scan(s.Path)
+	if len(secret) < 16 {
+		return ErrSecretTooShort
+	}
+	ko[s.Service] = secret
+
+	passcode, _ := otp.Generate(secret)
+	fmt.Println(passcode)
+
+	newStr, err := godotenv.Marshal(ko)
+	if err != nil {
+		return err
+	}
+
+	newBuf, err := crypto.Encrypt(psw, newStr)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(dataPath, []byte(newBuf), os.ModePerm)
+}
+
+func scan(path string) string {
+	// open and decode image file
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	img, _, err := image.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	// prepare BinaryBitmap
+	bmp, err := gozxing.NewBinaryBitmapFromImage(img)
+	if err != nil {
+		panic(err)
+	}
+
+	// decode image
+	qrReader := qrcode.NewQRCodeReader()
+	result, err := qrReader.Decode(bmp, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return result.GetText()
+}
